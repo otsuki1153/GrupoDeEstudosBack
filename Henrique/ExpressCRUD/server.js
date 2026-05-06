af417c4718451fede71d2959aef6e47bd0f0fbb9
Henrique <107266141+otsuki1153@users.noreply.github.com>
2026-05-06 13:41:55 -0300

    Add files via upload

---
+++ Henrique/ExpressCRUD/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import TaskRouter from './route/task.routes.js'
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/tasks", TaskRouter);
+
+app.listen(PORT, () =>{
+    console.log(`API rodando na porta ${PORT}`)
+});
