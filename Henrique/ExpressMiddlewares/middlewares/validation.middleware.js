8b0990299dabe8cb91cc76008d1a3813b7ed56ce
Henrique <107266141+otsuki1153@users.noreply.github.com>
2026-05-18 13:50:53 -0300

    Update validation.middleware.js

--- Henrique/ExpressMiddlewares/middlewares/validation.middleware.js
+++ Henrique/ExpressMiddlewares/middlewares/validation.middleware.js
@@ -1,15 +1,34 @@
+import {z} from 'zod';
+
+const TaskSchema = z.object({
+    title = z.string().trim().min(1, "O título é Obrigatório")
+});
 export default function validationMiddleware(req, res,next) {
     if(req.method === "GET" || req.method === "DELETE"){
         return next();
     }
-    if(!Object.hasOwn(req.body, 'title')){
-        return res.status(400).send("Error 400 Bad Request");
+
+    // const {title} = req.body;
+    // if(title === undefined){
+    //     return res.status(400).send("Error 400 Bad Request");
+    // }
+    // if(typeof title !== "string"){
+    //     return res.status(400).send("Error 400 Bad Request");
+    // }
+    // if(title.trim() === ""){
+    //     return res.status(400).send("Error 400 Bad Request");
+    // }
+
+    const result = TaskSchema.safeParse(req.body);
+
+    if(!result.success){
+        return res.status(400).json({
+            errors: result.error.issues.map(issues =>({
+                field: issue.path.join("."),
+                message: issue.message
+            }))
+        });
     }
-    if(typeof req.body.title !== "string"){
-        return res.status(400).send("Error 400 Bad Request");
-    }
-    if(req.body.title.trim() === ""){
-        return res.status(400).send("Error 400 Bad Request");
-    }
+    req.body = result.data;
     next();
 };
